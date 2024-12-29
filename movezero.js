343fdc6e93d30645a10573e76c0556ad55a8c92f
selvarajblr93 <selvarajblr93@gmail.com>
2024-12-29 10:44:39 +0000

    update

--- movezero.js
+++ movezero.js
@@ -1,1 +1,16 @@
 
+function moveZeroes(nums) {
+    let writePointer = 0;
+    for (let readPointer = 0; readPointer < nums.length; readPointer++) {
+        const val = nums[readPointer];
+        nums[readPointer] = 0;
+        if (val !== 0) {
+            nums[writePointer] = val;
+            writePointer++;
+        }
+    }
+
+    console.log(nums)
+}
+
+moveZeroes([1,2,3,0,4,0,5]);
